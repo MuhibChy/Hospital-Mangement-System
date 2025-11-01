3d96e86440c3941dac3ef15eee5b4abb18615178
MuhibChy <muhibchy90@gmail.com>
2025-11-01 16:24:18 +0000

    Fix GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const base = '/Hospital-Mangement-System/';
+    
     return {
-      base: '/Hospital-Mangement-System/',
+      base: base,
       server: {
         port: 3000,
         host: '0.0.0.0',
